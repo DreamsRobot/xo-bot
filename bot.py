d32efc64432b4472ced9a7300429a0685599e52d
Bazigar ( CFC ) <hdhindihub@gmail.com>
2025-08-30 04:30:38 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,14 @@
+from telegram.ext import Updater
+from config import BOT_TOKEN
+from handlers.commands import register_command_handlers
+from handlers.callback import register_callback_handlers
 
+def main():
+    updater = Updater(token=BOT_TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
+
+    register_command_handlers(dispatcher)
+    register_callback_handlers(dispatcher)
+
+    updater.start_polling()
+    updater.idle()
